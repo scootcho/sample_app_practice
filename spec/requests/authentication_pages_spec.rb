fef2fa8ec90371ff3bda808b57ad5866de512966
Scott Yu <scott.tj.yu@gmail.com>
2014-09-04 19:26:09 +0800

    requireing signedin user

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,24 +25,44 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
+  end
 
-    describe "with valid information" do
-      let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+  describe "with valid information" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      fill_in "Email",    with: user.email.upcase
+      fill_in "Password", with: user.password
+      click_button "Sign in"
+    end
 
-      it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
+    it { should have_title(user.name) }
+    it { should have_link('Profile',     href: user_path(user)) }
+    it { should have_link('Settings',    href: edit_user_path(user)) }
+    it { should have_link('Sign out',    href: signout_path) }
+    it { should_not have_link('Sign in', href: signin_path) }
 
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+    describe "followed by signout" do
+      before { click_link "Sign out" }
+      it { should have_link('Sign in') }
+    end
+  end
+  
+  describe "authroization" do
+
+    describe "for non-signed in users" do
+      let (:user) { FactoryGirl.create(user) }
+
+      describe "in the Users Controller" do
+
+        describe "visiting edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+        
+        describe "visit profile page" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
       end
     end
   end
